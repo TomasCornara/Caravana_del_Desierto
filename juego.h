8125b984f65c34c859f5ba61480ee37a3f5208ea
Tomás Andrés Cornara Perez <55905365+TomasCornara@users.noreply.github.com>
2026-05-11 17:52:16 -0300

    Estructuras del Juego

---
+++ juego.h
@@ -0,0 +1,54 @@
+#ifndef JUEGO_H_INCLUDED
+#define JUEGO_H_INCLUDED
+#include <stdbool.h>
+
+/*
+
+#include "lista_circular_doble"
+
+#define MAX_NOMBRE 4
+#define TIPO_INICIO 1
+#define TIPO_FIN 2
+#define TIPO_NORMAL 3
+#define TIPO_OASIS 4
+#define TIPO TORMENTA 5
+#define TIPO_VIDA_EXTRA 6
+#define TIPO_PREMIO 7
+
+
+typedef t_arbol t_raking;
+typedef t_listaCirDoble t_mapa;
+typedef t_mapa t_pos;
+
+typedef struct{
+    char nombre[MAX_NOMBRE];
+    unsigned vidas;
+    unsigned puntos;
+    bool efectoTormenta;
+    bool efectoOasis;
+    t_posicion pos;
+}t_jugador;
+
+typedef struct{
+    unsigned tipo_casillero;
+    bool presencia_jugador;
+    unsigned cant_bandidos;
+}t_casillero;
+
+typedef t_cola t_movimientos;
+
+typedef struct{
+    unsigned pos_inicial;
+    unsigned pos_final;
+    bool jugador_humano;
+}t_movimiento;
+
+typedef struct{
+    char puesto;
+    char nombre[MAX_NOMBRE];
+    unsigned puntos;
+}t_rankeo
+
+*/
+
+#endif // JUEGO_H_INCLUDED
