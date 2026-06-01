f256b62cee3d72fb46b07080ae4164700055ccfe
Matias Divano <75194265+Hosuru@users.noreply.github.com>
2026-06-01 18:16:47 -0300

    Cambio en los atributos de resolver_casillero_actual y resolver_bandido_en_casillero para acomodar los movimientos del bandido al jugador

--- juego.h
+++ juego.h
@@ -92,11 +92,9 @@ void inicializar_jugador(t_jugador *jugador, t_config *config, t_mapa mapa);
 int guardar_movimiento(t_movimientos *cola, unsigned pos_inicial, unsigned pos_final, bool jugador_humano);
 void mover_jugador(t_jugador *jugador, unsigned pasos, t_movimientos *cola_movimientos, int lado);
 void mover_bandido(t_mapa *mapa, t_movimientos *cola_movimientos);
-void resolver_casillero_actual(t_jugador *jugador, t_casillero *casillero_actual);
-void resolver_bandido_en_casillero(t_jugador *jugador, t_casillero *casillero_actual);
+void resolver_casillero_actual(t_jugador *jugador, t_casillero *casillero_actual, t_movimientos *cola_movimientos);
+void resolver_bandido_en_casillero(t_jugador *jugador, t_casillero *casillero_actual, t_movimientos *cola_movimientos);
 void mostrarMapa(t_mapa *mapa);
-void resolver_casillero_actual(t_jugador *jugador, t_casillero *casillero_actual);
-void resolver_bandido_en_casillero(t_jugador *jugador, t_casillero *casillero_actual);
 
 
 #endif // JUEGO_H_INCLUDED
